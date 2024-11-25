0b0976545c6e7175184c0adb823ee17149d4b16d
Sally Kolodny <sallykolodny@protonmail.com>
2024-11-25 02:10:58 -0500

    Updating code...

---
+++ assets/js/StackedSharesFound.js
@@ -0,0 +1,95 @@
+const csvUrl = '/data/stacked_shares_found.csv';
+const dateData = [];
+const totalData = [];
+
+Papa.parse(csvUrl, {
+  download: true,
+  header: true,
+  delimiter: ",",
+  complete: data => {
+    data.data.forEach(row => {
+      const dateString = row['Date'];
+      const value = row['Shares Found'];
+
+      // Check for missing or invalid data
+      if (!dateString || isNaN(value)) {
+        console.log('Invalid or missing data found. Skipping this row.');
+        return;
+      }
+
+      // Parse the date string and convert it to a timestamp
+      const date = new Date(dateString).getTime();
+
+      dateData.push(date);
+      //totalData.push(Number(total));
+      totalData.push({ x: date, y: value });
+
+    });
+    
+    const areaOptions = {
+      chart: {
+        id: "barChart",
+        type: "bar",
+        height: 275,
+        foreColor: "#ccc",
+        toolbar: {
+          autoSelected: "pan",
+          show: false
+        }
+      },
+      colors: ["#00baec"],
+      stroke: {
+	    curve: 'stepline',
+        width: 3
+      },
+      grid: {
+        borderColor: "#555",
+        clipMarkers: false,
+        yaxis: {
+          lines: {
+            show: false
+          }
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      fill: {
+        gradient: {
+          enabled: true,
+          opacityFrom: 0.55,
+          opacityTo: 0,
+        }
+      },
+      markers: {
+        size: 0,
+        colors: ["#000524"],
+        strokeColor: "#00baec",
+        strokeWidth: 1
+      },
+      series: [
+        {
+          name: "Shares Found",
+          data: totalData
+        }
+      ],
+      tooltip: {
+        theme: "dark"
+      },
+      title: {
+	    text: 'Shares Found',
+	    align: 'left'
+      },
+      xaxis: {
+        type: "datetime"
+      },
+      yaxis: {
+        min: 0
+      }
+    };
+
+    var areaChart = new ApexCharts(document.querySelector("#areaChart"), areaOptions);
+    areaChart.render();
+
+  }});
+
---
+++ pages/Stacked-Shares-Found.md
@@ -0,0 +1,17 @@
+---
+layout: post
+title: Stacked Shares Found
+date: 2024-11-25
+---
+<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.0/papaparse.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="/assets/js/StackedSharesFound.js"></script>
+
+The chart below shows a visualization of the Monero XMR shares that were found on the mini sidechain.
+
+<div id="wrapper">
+  <div id="areaChart">
+  </div>
+ </div>
+
+
